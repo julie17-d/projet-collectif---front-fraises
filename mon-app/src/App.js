87d80bfc661219f6e3f7ad4e55885abd566609c8
julie17-d <derbaljulie@gmail.com>
2022-11-02 15:24:27 +0100

    cards meubles

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -4,6 +4,34 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function displayMeubles() {
+  let meubles = {
+    1: "chaise",
+    2: "table",
+    3: "tabouret",
+    4: "papier peint"
+  };
+  const arr = [];
+  for (let meuble in meubles) {
+      arr.push(
+        <Card style={{ width: '18rem' }} className='card'>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{meubles[meuble]}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      );
+  }
+  return arr;
+}
+
+
+
 function App() {
   return (
     <div className="App">
@@ -22,18 +50,10 @@ function App() {
           fraises
         </a>
       </header>
-      <body>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+      <body className='App-body'>
+      
+          {displayMeubles()}
+        
       </body>
     </div>
   );
