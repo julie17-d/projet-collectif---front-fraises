37b3c7a65015631d9ee3e5c1d2c284bb2f6ec4d2
mebafares <faris.mebarakou.faris@gmail.com>
2022-11-04 10:40:52 +0100

    premier commit

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,41 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Cancel from './pages/Cancel';
+import Store from './pages/Store';
+import Success from './pages/Success';
+import CartProvider from './CartContext';
+// localhost:3000 -> Home
+// localhost:3000/success -> Success
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="danger">TEST</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          fraises
-        </a>
-      </header>
-      <body>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </body>
-    </div>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store />} />
+            <Route path="success" element={<Success />} />
+            <Route path="cancel" element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
