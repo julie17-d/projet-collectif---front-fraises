9d90c9483cd3d88d03bf5dee03f2b20eff867767
BAH ASSETOU <>
2022-11-04 10:39:18 +0100

    add script panier

---
+++ mon-app/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css';
+const Home = () => {
+  return (
+    <div className='container-fluid'>
+      <h3 className='text-center mt-5 text-uppercase'>Home page</h3>
+      <div className="container py-4">
+        <div className="container row">
+          
+        </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Home
