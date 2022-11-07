1845eb363caf7a831e5e728576be7acc33a23f91
Alexluu13 <alexluu13@gmail.com>
2022-11-07 11:00:50 +0100

    update app.js

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,40 +1,25 @@
-/* import logo from './logo.svg';
- */import './App.css';
+import './App.css';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/Navbar';
-import { Container } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Cancel from './pages/Cancel';
-import Store from './pages/Store';
-import Success from './pages/Success';
-import CartProvider from './CartContext';
-import Row from 'react-bootstrap/Row';
 import Furnitures from './components/Furnitures';
+import Cart from './components/Cart';
+
+
 
 function App() {
 
   //récupère les deux données du component Furnitures.jsx
-  const [furnitures, cart] = Furnitures()
+  const [furnitures, cartContent] = Furnitures()
   
   return (
     <div className="App">
-      <CartProvider>
-        <Container>
-          <NavbarComponent></NavbarComponent>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<Store />} />
-              <Route path="success" element={<Success />} />
-              <Route path="cancel" element={<Cancel />} />
-            </Routes>
-          </BrowserRouter>
-        </Container>
-      </CartProvider>
+      <header>
+        <Cart cart={JSON.stringify(cartContent)}/>
+      
+      </header>
       
       <body className='App-body'>
-        {console.log(cart)}
-        {JSON.stringify(cart)}
+        {console.log(cartContent)}
         <Row xs={1} md={3} className="gridCards">
           {furnitures}
         </Row>
