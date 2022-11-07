849a132f43b5ef61f180dfb1c6d7a173a03a0686
Julie <jem.baret@gmail.com>
2022-11-07 15:35:06 +0100

    add: first connection between front and backend on /api/furnitures endpoint

--- mon-app/src/components/Furnitures.jsx
+++ mon-app/src/components/Furnitures.jsx
@@ -1,15 +1,23 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import json from '../mock.json';
-import { useState } from 'react';
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 
 function Furnitures() {
   //initialisation de la variable d'état "cart"
   const [cart, setCart] = useState([]);
 
+  const [data, setData] = useState([]);
+  useEffect( () => {
+    axios.get("http://localhost:3001/api/furnitures")
+    .then((res) => setData(res.data));
+  }, []);
+  console.log(data)
+  
+
   //map sur les meubles
-  const furnitures = json.map((data) => {
+  const furnitures = data.map((item) => {
 
     //return l'élément JSX pour afficher les cards des meubles
     return (
@@ -17,24 +25,24 @@ function Furnitures() {
         <Card style={{ width: '24rem' }} className='card' border="dark" bg="dark" text="light">
           <Card.Header>
             <Card.Title>
-              <h3>{data.title}</h3>
+              <h3>{item.title}</h3>
             </Card.Title>
           </Card.Header>
-          <Card.Img className="card-img" src={data.pictureUrl} variant="top" />
+          <Card.Img className="card-img" src={item.pictureUrl} variant="top" />
           <Card.Body>
             <Card.Text>
-              {data.description}
+              {item.description}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            {data.price}€<br></br>
+            {item.price}€<br></br>
             <Button variant="secondary" onClick={() => {
 
               //onclick, crée un object avec infos du meuble...
               const object = {
-                id: data._id,
-                title: data.title,
-                price: data.price
+                id: item._id,
+                title: item.title,
+                price: item.price
               };
 
               //...et mets à jour "cart" en rajoutant à la fin de la liste le dernier object cliqué
