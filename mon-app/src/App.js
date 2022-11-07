e5dcc884227d68b6a064894bef88e7c0c3a4f078
julie17-d <derbaljulie@gmail.com>
2022-11-07 12:10:05 +0100

    add: display cart elements

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -9,17 +9,22 @@ function App() {
   //récupère les deux données du component Furnitures.jsx
   const [furnitures, cartContent] = Furnitures();
 
+  //map sur cartContent pour afficher le contenu du panier
+  const displayCartContent = cartContent.map((meuble) => {
+    return (meuble.title + " " + meuble.price + "€")
+  })
+
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <Navbar>
-        
         </Navbar>
+        {/* passe la const displayCartContent comme propriété de Cart pour afficher les meubles à l'intérieur du panier */}
+        <Cart cart={displayCartContent} />
       </header>
 
       <body className="App-body">
-        {console.log(cartContent)}
-  <Cart cart={JSON.stringify(cartContent)} /> 
+        {/* affiche meubles dans une grid */}
         <Row xs={1} md={3} className="gridCards">
           {furnitures}
         </Row>
